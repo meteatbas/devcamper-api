89ff0759f54590066696af9a61bc5d20aede5b05
meteatbs <130202020@kocaeli.edu.tr>
2020-06-20 16:56:19 +0300

    Added readme and starter config

---
+++ models/User.js
@@ -0,0 +1,77 @@
+const mongoose=require('mongoose');
+const crypto=require('crypto');
+const bcrypt = require('bcryptjs');
+const jwt=require('jsonwebtoken');
+
+const UserSchema=new mongoose.Schema({
+name:{
+    type:String,
+    required:[true,'Please add a name']
+},
+email:{
+    type:String,
+    required:[true,'Please add an email'],
+    unique:true,
+    match:[
+        /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/,
+        'Please add a valid email'
+    ]},
+    role:{
+        type:String,
+        enum:['user','publisher'],
+        default:'user'
+    },
+    password:{
+        type:String,
+        required:[true,'Please add a password'],
+        minlength:6,
+        select:false//we dont want to return with auth/me
+    },
+    resetPasswordToken:String,
+    resetPasswordExpire:Date,
+    createdAt:{
+        type:Date,
+        default:Date.now
+    }
+});
+
+//Encrypt password using bcrypt
+
+UserSchema.pre('save',async function(next){
+    if (!this.isModified('password')) {
+        next();
+    }
+   const salt = await bcrypt.genSalt(10);
+   this.password = await bcrypt.hash(this.password,salt);
+});
+
+//Sign JWT and return
+UserSchema.methods.getSignedJwtToken=function(){
+    return jwt.sign({id:this._id},process.env.JWT_SECRET,{
+        expiresIn:process.env.JWT_EXPIRE
+    })  //current user
+};
+
+//Match user entered password to hashed password int database
+UserSchema.methods.matchPassword=async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword,this.password);
+};
+
+//Generate and hash password token
+UserSchema.methods.getResetPasswordToken=function(){
+    //Generate token
+    const resetToken=crypto.randomBytes(20).toString('hex');
+
+    
+    
+    //Hash token and set to resetPasswordToken field
+    this.resetPasswordToken=crypto.createHash('sha256').update(resetToken).digest('hex');
+
+    //Set expire
+    this.resetPasswordExpire=Date.now()+10*60*1000;
+
+    return resetToken;
+};
+
+
+module.exports=mongoose.model('User',UserSchema);
--- server.js
+++ server.js
@@ -1,12 +1,100 @@
+const path=require('path');
 const express=require('express');
 const dotenv=require('dotenv');
+const morgan=require('morgan');
+const connectDB=require('./config/db');
+const fileupload=require('express-fileupload');
+const cookieParser=require('cookie-parser');
+const errorHandler=require('./middleware/error')
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss=require('xss-clean');
+const hpp=require('hpp');
+const cors=require('cors');
+const ratelimit=require('express-rate-limit');
 
 //Load env variables
 dotenv.config({path:'./config/config.env'});
 
+//Connect to db
+connectDB();
+
+
+//Route files
+const bootcamps=require('./routes/bootcamps');
+const courses=require('./routes/courses');
+const auth=require('./routes/auth'); 
+const users=require('./routes/users'); 
+const reviews=require('./routes/reviews'); 
+const rateLimit = require('express-rate-limit');
+
 const app =express();
+
+
+//Body parser
+app.use(express.json());
+
+
+//Cookie parser 
+app.use(cookieParser());
+
+
+//Dev logging middleware
+if (process.env.NODE_ENV==='development') {
+    app.use(morgan('dev'));
+}
+
+//File uploading 
+app.use(fileupload());
+
+//Sanitize data
+app.use(mongoSanitize());
+
+//Prevent XSS attacks
+app.use(xss());
+
+//Set security headers
+app.use(helmet());
+
+//Rate limiting
+const limiter=rateLimit({
+    windowMs:10*60*1000, //10 minutes
+    max:100
+})
+
+app.use(limiter);
+
+//prevent http param pollution
+app.use(hpp());
+
+
+//Enable CORS
+app.use(cors());
+
+//Set static folder
+app.use(express.static(path.join(__dirname,'public')));
+
+
+//Mount routers
+app.use('/api/v1/bootcamps',bootcamps);
+app.use('/api/v1/courses',courses);
+app.use('/api/v1/auth',auth);
+app.use('/api/v1/users',users);
+app.use('/api/v1/reviews',reviews);
+
+app.use(errorHandler);
+
+
 
 const PORT=process.env.PORT || 5000;
 
-app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server=app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
 
+//Handle unhandled promise rejections
+
+process.on('unhandledRejection',(err,promise)=>{
+    console.log(`Error: ${err.message}`);
+    //close server and exit process
+    server.close(()=>process.exit(1));
+    
+});
